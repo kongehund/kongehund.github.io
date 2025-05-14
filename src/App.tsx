d82a65499cdcd06c61f6e5a249260142c1ad8681
kongehund <63306696+kongehund@users.noreply.github.com>
2025-05-14 20:52:35 +0200

    attempt fix to publish to github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, HashRouter } from 'react-router-dom';
 
 import './App.css';
 import Homepage from './pages/Homepage/Homepage';
@@ -6,10 +6,10 @@ import Tempora from './pages/Tempora/Tempora';
 
 function App() {
   return (
-    <Routes>
+    <HashRouter>
       <Route path="/" element={<Homepage />} />
       <Route path="/tempora" element={<Tempora />} />
-    </Routes>
+    </HashRouter>
   )
 }
 
