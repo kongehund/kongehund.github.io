1766345f6a6f6cb234d86a3ba2142deca8fc7d7d
kongehund <63306696+kongehund@users.noreply.github.com>
2025-05-14 20:55:27 +0200

    replace BrowserRouter with HashRouter for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, HashRouter } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 import './App.css';
 import Homepage from './pages/Homepage/Homepage';
@@ -6,10 +6,10 @@ import Tempora from './pages/Tempora/Tempora';
 
 function App() {
   return (
-    <HashRouter>
+    <Routes>
       <Route path="/" element={<Homepage />} />
       <Route path="/tempora" element={<Tempora />} />
-    </HashRouter>
+    </Routes>
   )
 }
 
